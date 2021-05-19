0815e02a18ef3f316a199ecae94833a90eb3f461
jimin <jiminaleejung@gmail.com>
2021-05-19 17:50:54 +0900

    Feat: Test 데이터 테이블 생성 및 superuser 계정 생성

--- backend/mypage/admin.py
+++ backend/mypage/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TestProduct, TestUser
 
-# Register your models here.
+admin.site.register(TestProduct)
+admin.site.register(TestUser)
--- backend/mypage/tests.py
+++ backend/mypage/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
+from .models import TestProduct, TestUser
 
-# Create your tests here.
