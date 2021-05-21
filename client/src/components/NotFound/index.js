41f88ba0eee4552e2ac6666a797b58bb49b61879
marxl <isl9702@naver.com>
2021-05-21 23:59:58 +0900

    feat: NotFound 페이지 추가

---
+++ client/src/components/NotFound/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh',
+      }}
+    >
+      <h1>NotFound</h1>
+    </div>
+  );
+}
