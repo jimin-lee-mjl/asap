2eae51083b045d28704b09303404f11687a2530c
marxl <isl9702@naver.com>
2021-05-22 00:00:36 +0900

    feat: 라우팅 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 import UserInfo from './components/UserInfo';
+import MyPage from './components/MyPage';
+import NotFound from './components/NotFound';
 
 export default function App() {
+  const [user, setUser] = useState(null); // 로그인 된 사용자 정보
+  const authenticated = user != null; // 로그인 된 사용자가 존재하는지, 인증 여부를 저장
+
+  console.log(authenticated);
+
+  // const login = ({ email, password }) => setUser(signIn({ email, password }));
+  const login = () => {};
+  const logout = () => setUser(null);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/hello')
+      .then((response) => console.log(response.data));
+  }, []);
+
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/mypage" component={MyPage} />
         <Route path="/info" component={UserInfo} />
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
