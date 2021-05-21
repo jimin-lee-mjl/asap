6b3dd4c30802df586dc187cdb6df472123adfeb3
marxl <isl9702@naver.com>
2021-05-21 11:46:08 +0900

    feat: 마이페이지 내용 일부 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import UserInfo from './components/UserInfo';
+import MyPage from './components/MyPage';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/info" component={UserInfo} />
+        <Route path="/mypage" component={MyPage} />
       </Switch>
     </Router>
   );
