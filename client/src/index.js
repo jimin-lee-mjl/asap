f5be26818bda993fb6364cd36f8b517fa6b3eb40
marxl <isl9702@naver.com>
2021-05-25 16:53:33 +0900

    feat: react app과 redux 연결

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
