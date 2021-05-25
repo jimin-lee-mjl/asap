d6cf1591db725a7cf91feb70b42577abb5e6ba60
jimin <jiminaleejung@gmail.com>
2021-05-25 15:39:52 +0900

    Docs: API 문서 커스터마이징

--- backend/mypage/views.py
+++ backend/mypage/views.py
@@ -2,16 +2,44 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from .models import TestProduct, TestUser
 from .serializers import ItemSerializer, UserSerializer
 
 
 class ProfileView(APIView):
+    '''
+    사용자 프로필을 조회하고 업데이트하는 API
+
+    ---
+    ## `/api/mypage/<int:user_id>/profile`
+    ## 내용
+       - name : 유저 닉네임
+       - wish : 찜 목록
+       - bag : 장바구니 목록
+       - buy : 구매 내역 
+    '''
+    profile_response = {
+        '204': openapi.Response(
+            description='사용자 프로필 정보를 업데이트하는 API',
+            examples={
+                'application/json': {
+                    'name': '앨리스',
+                    'wish': ['item1', 'item2'],
+                    'bag': ['item3', 'item4'],
+                    'buy': ['item5', 'item6']
+                }
+            }
+        )
+    }
+
+    @swagger_auto_schema(responses=profile_response)
     def get(self, request, user_id, format=None):
         user = get_object_or_404(TestUser, pk=user_id)
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
     def patch(self, request, user_id, format=None):
         serializer = UserSerializer(data=request.data, partial=True)
 
@@ -26,6 +54,14 @@ class ProfileView(APIView):
 
 
 class WishListView(APIView):
+    '''
+    사용자의 찜 목록에서 아이템을 삭제하는 API
+
+    ---
+    ## `api/mypage/<int:user_id>/wishlist/`
+    ## 내용
+       - name : 아이템 이름
+    '''
     def delete(self, request, user_id, format=None):
         serializer = ItemSerializer(data=request.data)
 
@@ -40,6 +76,14 @@ class WishListView(APIView):
 
 
 class BagView(APIView):
+    '''
+    사용자의 장바구니 목록에서 아이템을 삭제하는 API
+
+    ---
+    ## `api/mypage/<int:user_id>/bag/`
+    ## 내용
+       - name : 아이템 이름
+    '''
     def delete(self, request, user_id, format=None):
         serializer = ItemSerializer(data=request.data)
 
