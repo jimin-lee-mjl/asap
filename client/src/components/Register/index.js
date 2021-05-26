ea0f900ac49feaa6bdeff1025f5076221b8b4d77
marxl <isl9702@naver.com>
2021-05-26 16:21:38 +0900

    feat: 회원가입 페이지 css

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { register } from '../../actions/auth';
+import styled from 'styled-components';
 
 function RegisterPage(props) {
   const [username, setName] = useState('');
@@ -44,33 +45,104 @@ function RegisterPage(props) {
         height: '100vh',
       }}
     >
-      <form
-        onSubmit={onSubmitHandler}
-        style={{ display: 'flex', flexDirection: 'column' }}
-      >
-        <h1>Create Account</h1>
+      <Container>
+        <form
+          onSubmit={onSubmitHandler}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '30px 40px',
+          }}
+        >
+          <h1 style={{ textAlign: 'center', margin: '0 0 20px' }}>
+            Create Account
+          </h1>
 
-        <label>Name</label>
-        <input type="test" value={username} onChange={onNameHandler} />
+          <FormControl>
+            <label>Name</label>
+            <input type="test" value={username} onChange={onNameHandler} />
+          </FormControl>
 
-        <label>Password</label>
-        <input type="password" value={password1} onChange={onPasswordHanlder} />
+          <FormControl>
+            <label>Password</label>
+            <input
+              type="password"
+              value={password1}
+              onChange={onPasswordHanlder}
+            />
+          </FormControl>
 
-        <label>Confirm Pasword</label>
-        <input
-          type="password"
-          value={password2}
-          onChange={onConfirmPasswordHandler}
-        />
+          <FormControl>
+            <label>Confirm Pasword</label>
+            <input
+              type="password"
+              value={password2}
+              onChange={onConfirmPasswordHandler}
+            />
+          </FormControl>
 
-        <label>Email (선택)</label>
-        <input type="email" value={email} onChange={onEmailHandler} />
-        <br />
+          <FormControl>
+            <label>Email (선택)</label>
+            <input type="email" value={email} onChange={onEmailHandler} />
+            <br />
+          </FormControl>
 
-        <button type="submit">회원 가입</button>
-      </form>
+          <Button type="submit">회원 가입</Button>
+        </form>
+      </Container>
     </div>
   );
 }
 
 export default withRouter(RegisterPage);
+
+const Container = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+  font-family: 'Noto Sans KR', sans-serif;
+  width: 400px;
+  border-radius: 0.6em;
+  padding: 20px;
+  background-color: white;
+  box-shadow: 20px 20px 60px #97a1a1, -20px -20px 60px #ffffff;
+  margin: 20px auto;
+`;
+
+const FormControl = styled.div`
+  margin-bottom: 10px;
+  padding-bottom: 5px;
+  position: relative;
+
+  label {
+    color: #33032d;
+    display: block;
+    margin-bottom: 5px;
+  }
+
+  input {
+    border-radius: 6px;
+    min-height: 2.618em;
+    border: gray solid 2px;
+    display: block;
+    width: 100%;
+    font-size: 14px;
+    padding: 7px;
+  }
+`;
+
+const Button = styled.div`
+  background: black;
+  color: white;
+  text-align: center;
+  vertical-align: middle;
+  display: table-cell;
+  line-height: 2.5;
+  padding: 7px;
+  border-radius: 0.6em;
+  border: solid white 2px;
+
+  :hover {
+    color: black;
+    background: white;
+    border: solid black 2px;
+  }
+`;
