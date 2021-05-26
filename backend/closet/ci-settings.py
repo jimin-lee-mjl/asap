5f064804f5a5f2e70cf4ae357f682aa3f9b16a12
jimin <jiminaleejung@gmail.com>
2021-05-27 02:42:12 +0900

    Fix: ci-settings 파일 수정

--- backend/closet/ci-settings.py
+++ backend/closet/ci-settings.py
@@ -1,9 +1,4 @@
 from pathlib import Path
-import environ
-
-# 환경 변수 초기화
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = '7=7Aif=M&]o6XD4w@e1m"pjT~zLX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
