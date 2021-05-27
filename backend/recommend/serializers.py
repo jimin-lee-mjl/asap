801b2c0f559bfc71620c4a0593d13727f9614724
leehyeokjun <asymptote@live.co.kr>
2021-05-27 15:39:26 +0900

    commit backend/recommend

---
+++ backend/recommend/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Amazon
+
+class AmazonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Amazon
+        fields = ['asin']
