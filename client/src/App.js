68fcd945e28aadbfa2a9c7ba85bf84d6bab933e0
marxl <isl9702@naver.com>
2021-05-28 11:54:20 +0900

    feat: 마이페이지 정적 ui 완성

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import Home from './components/Home';
 import Recommend from './components/Recommend';
@@ -8,27 +9,17 @@ import Result from './components/Result';
 import Login from './components/Login';
 import Register from './components/Register';
 import UserInfo from './components/UserInfo';
-import MyPage from './components/MyPage';
+import MyPage from './components/MyPage/';
 import NotFound from './components/NotFound';
 import AuthRoute from './components/AuthRoute';
-
 
 export default function App() {
   const [user, setUser] = useState(null); // 로그인 된 사용자 정보
   const authenticated = user != null; // 로그인 된 사용자가 존재하는지, 인증 여부를 저장
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  console.log('뭬', isAuthenticated);
 
   console.log(authenticated);
-
-  // const login = ({ email, password }) => setUser(signIn({ email, password }));
-  const login = () => {};
-  const logout = () => setUser(null);
-
-  // backend 통신 테스트 코드
-  useEffect(() => {
-    axios
-      .get('http://localhost:5000/api/hello')
-      .then((response) => console.log(response.data));
-  }, []);
 
   return (
     <Router>
@@ -37,12 +28,11 @@ export default function App() {
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <AuthRoute
-          authenticated={authenticated}
+          authenticated={isAuthenticated}
           path="/mypage"
           render={(props) => <MyPage user={user} {...props} />}
         />
         <Route path="/info" component={UserInfo} />
-        <Route path="/mypage" component={MyPage} />
         <Route path="/recommend" component={Recommend} />
         <Route path="/result" component={Result} />
         <Route component={NotFound} />
--- client/src/components/MyPage/index.js
+++ client/src/components/MyPage/index.js
@@ -1,119 +1,223 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Avatar } from 'antd';
+import axios from 'axios';
+import { Card } from 'antd';
 
-export default function MyPage({ user }) {
+function ItemCard({ productId }) {
+  const { Meta } = Card;
+
+  const [item, setItem] = useState({});
+
+  const fetchProductDetail = async () => {
+    const response = await axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .catch((err) => console.log(err));
+    setItem(response.data);
+  };
+
+  useEffect(() => {
+    if (productId && productId !== '') {
+      fetchProductDetail();
+    }
+  }, [productId]);
+
   return (
-    <Container>
-      <Nav>
-        <button>Home</button>
-      </Nav>
-      <Header>
-        <h1>My Page</h1>
-      </Header>
-      <Profile>
-        <h1>Profile</h1>
-        <Avatar shape="square" style={{ backgroundColor: 'gray' }} size={64}>
-          User
-        </Avatar>
-        <article>
-          <h2>ID: elice</h2>
-          <h2>Password: ********* </h2>
-          <h2>Email: elice@elice.com </h2>
-          <h2>Gender: Women </h2>
-          <h2>age: 20 </h2>
-          <h2>Size: M </h2>
-        </article>
-        <button>edit</button>
-      </Profile>
-      <Keyword>Keyword</Keyword>
-      <Color>Color</Color>
-      <Budget>Budget</Budget>
-      <Liked>
-        Liked
-        <button>더보기</button>
-        <div></div>
-      </Liked>
-      <Purchased>
-        Purchased
-        <button>더보기</button>
-      </Purchased>
-    </Container>
+    <>
+      {Object.keys(item).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div
+          style={{
+            width: '240px',
+            height: '380px',
+            border: '1px solid black',
+            margin: '1rem',
+          }}
+        >
+          <img
+            src={item.image}
+            alt={item.title}
+            style={{ width: '230px', height: '300px', objectFit: 'contain' }}
+          />
+          <p>
+            {item.id}.{item.title}
+          </p>
+          {/* <Card
+            hoverable
+            style={{ width: 240, height: 400 }}
+            cover={<img alt={item.id} src={item.image} />}
+          >
+            <Meta title={item.title} />
+          </Card> */}
+        </div>
+      )}
+    </>
   );
 }
 
-const Container = styled.div`
-  display: grid;
-  width: 80vw;
-  height: 80vh;
-  border: solid 1px black;
-  grid-template-columns: 1fr 0.5fr 0.5fr 1fr;
-  grid-template-rows: 0.4fr 0.6fr 3fr 3fr;
-  grid-template-areas:
-    'nav nav nav nav'
-    'header header header header'
-    'profile keyword keyword liked'
-    'profile color budget purchased';
-  grid-gap: 0.2rem;
+function MyPage() {
+  const keywords = ['fit', 'office', 'great', 'awesome'];
+  const asins = [1, 2, 3, 4, 5, 6, 7, 8];
+  const [items, setItems] = useState([]);
 
-  @media only screen and (max-width: 550px) {
-    width: 100vw;
-    height: 100vh;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 0.4fr 0.4fr 2.2fr 1.2fr 1.2fr 1.2fr 1.2fr;
-    grid-template-areas:
-      'nav nav'
-      'header header'
-      'profile profile'
-      'keyword keyword'
-      'color budget'
-      'liked liked'
-      'purchased purchased';
-  }
+  useEffect(() => {
+    const res = asins.map((asin) => {
+      return fetchItems(asin);
+    });
+    console.log('resres', res);
+    setItems(res);
+  }, []);
+
+  useEffect(() => {
+    console.log('@@', items);
+  }, [items]);
+
+  const fetchItems = (asin) => {
+    let response;
+    fetch(`https://fakestoreapi.com/products/${asin}`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setItems([...items, json]);
+        console.log('ttt', items);
+      });
+    console.log(response);
+    return response;
+  };
+
+  return (
+    <>
+      <Header />
+      <Title>My Page</Title>
+      <Container>
+        <Content>
+          <h1>Information</h1>
+          <InformationBox>
+            I'm <B>elice22✋</B>! <br />
+            Interested in <B>WOMAN'S FASHION👗</B> <br />
+            Usually shopping with <B>$300💰</B>
+          </InformationBox>
+        </Content>
+        <Content>
+          <h1>Favorite Color</h1>
+          <img
+            style={{
+              width: '15vw',
+              height: '25vh',
+              objectFit: 'cover',
+            }}
+            alt="green"
+            src="https://images.unsplash.com/photo-1536147116438-62679a5e01f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+          />
+        </Content>
+        <Content>
+          <h1>Keywords</h1>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {keywords.map((word) => {
+              return (
+                <div
+                  style={{
+                    backgroundColor: 'rgba(196, 196, 196, 0.4)',
+                    width: '7rem',
+                    height: '1.5rem',
+                    marginLeft: '0.5rem',
+                  }}
+                >
+                  {word}
+                </div>
+              );
+            })}
+          </div>
+        </Content>
+        <Content>
+          <h1>Liked Items</h1>
+          <ItemCards>
+            {asins.map((n) => {
+              return <ItemCard productId={n}></ItemCard>;
+            })}
+          </ItemCards>
+        </Content>
+      </Container>
+    </>
+  );
+}
+
+export default MyPage;
+
+const Header = styled.div`
+  position: absolute;
+  top: 0;
+  background-image: url('header.jpg');
+  width: 100vw;
+  height: 20vh;
+  background-repeat: repeat;
 `;
 
-const Nav = styled.nav`
-  background: #a7ffeb;
-  grid-area: nav;
+const Title = styled.div`
+  background-image: url('orange.png');
+  position: absolute;
+  top: 15vh;
+  width: 30vw;
+  height: 13vh;
   text-align: right;
+  @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+  font-family: 'Pacifico', cursive;
+  font-size: 2rem;
+  padding-top: 3vh;
+  padding-right: 20px;
 `;
 
-const Header = styled.main`
-  background: #81ecec;
-  grid-area: header;
+const Container = styled.div`
+  margin-top: 30vh;
+  width: 100%;
+  height: auto;
+  text-align: center;
+  position: absolute;
+  @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+  font-family: 'Pacifico', cursive;
+`;
+
+const Content = styled.div`
+  margin-bottom: 5vh;
   text-align: center;
 `;
 
-const Profile = styled.div`
-  background: #80deea;
-  grid-area: profile;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+const InformationBox = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+  font-family: 'Noto Sans KR', sans-serif;
+  background-color: rgba(196, 196, 196, 0.4);
+  width: 40vw;
+  height: auto;
+  margin: auto;
+  padding: 1.5rem;
+`;
+
+const B = styled.h3`
+  font-weight: bold;
+  display: inline;
+`;
+
+const ColorImg = styled.img`
+  width: 15vw;
+  height: 25vh;
 `;
 
 const Keyword = styled.div`
-  background: #80deea;
-  grid-area: keyword;
+  background-color: rgba(196, 196, 196, 0.4);
 `;
 
-const Color = styled.div`
-  background: #80deea;
-  grid-area: color;
-`;
-
-const Budget = styled.div`
-  background: #80deea;
-  grid-area: budget;
-`;
-
-const Liked = styled.div`
-  background: #80deea;
-  grid-area: liked;
-`;
-
-const Purchased = styled.div`
-  background: pink;
-  grid-area: purchased;
+const ItemCards = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+  font-family: 'Noto Sans KR', sans-serif;
+  display: flex;
+  align-items: center;
+  width: 1100px;
+  flex-wrap: wrap;
+  margin: auto;
 `;
