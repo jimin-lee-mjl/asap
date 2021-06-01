a84b63e365a274b28f7b85864e546e6b9ea92473
jimin <jiminaleejung@gmail.com>
2021-06-01 19:52:53 +0900

    Feat: view 파일 분리

--- backend/mypage/urls.py
+++ backend/mypage/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from . import views
+from .views import user_views
+from .views import order_views
+from .views import item_views
 
 app_name = 'mypage'
 
 urlpatterns = [
-    path('user/<int:user_id>/', views.UserDetailListView.as_view(), name='detail'),
-    path('user/<int:user_id>/delivery/', views.DeliveryInfoSaveView.as_view(), name='delivery'),
-    path('user/<int:user_id>/like/', views.LikeItemDetailView.as_view(), name='like'),
-    path('user/<int:user_id>/cart/', views.CartItemDetailView.as_view(), name='cart'),
-    path('order/detail/<int:order_id>/', views.ListOrderDetailView, name='order_detail'),
-    path('order/<int:user_id>/', views.CreateNewOrderView, name='new_order')
+    path('user/<int:user_id>/', user_views.UserDetailListView.as_view(), name='detail'),
+    path('user/<int:user_id>/delivery/', user_views.DeliveryInfoSaveView.as_view(), name='delivery'),
+    path('user/<int:user_id>/like/', item_views.LikeItemDetailView.as_view(), name='like'),
+    path('user/<int:user_id>/cart/', item_views.CartItemDetailView.as_view(), name='cart'),
+    path('order/detail/<int:order_id>/', order_views.ListOrderDetailView, name='order_detail'),
+    path('order/<int:user_id>/', order_views.CreateNewOrderView, name='new_order')
 ]
