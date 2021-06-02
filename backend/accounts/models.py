3f45cd772d87a8e384ff1501a3edc5b33daf9f67
kimhyunkwang <khkhy8712@gmail.com>
2021-06-02 13:47:06 +0900

    Style: autopep8 적용

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,17 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 
 # Create your models here.
+
+
 class User(AbstractUser):
-    # username = None
-    # first_name = None
-    # last_name = None
-    # email = models.EmailField('이메일', unique=True)
-    # password = models.CharField('비밀번호', max_length=150)
-
-    # # USERNAME_FIELD = 'email'
-    # REQUIRED_FIELD = []
-
-    # objects = UserManager()
-
-    address = models.CharField('주소', blank=True, max_length=200)
-    # pass
+    address = models.CharField('Address', blank=True, max_length=200)
+    # like_items = models.ManyToManyField('Item', blank=True)
+    # cart_items = models.ManyToManyField('Item', blank=True)
+    # keywords = models.ManyToManyField('Keyword', blank=True)
