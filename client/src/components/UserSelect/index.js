def685cba5419107a57e8efe0597745aecffa982
irisdew <isl9702@naver.com>
2021-06-04 15:14:27 +0900

    Feat: antd Steps 커스텀 css

--- client/src/components/UserSelect/index.js
+++ client/src/components/UserSelect/index.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { Steps, Button, message } from 'antd';
+import { Steps, message } from 'antd';
 import Personal from './Personal';
 import Category from './Category';
 import Keyword from './Keyword';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import HeaderComponent from '../Header';
+
+import Gender from './Gender';
 
 const { Step } = Steps;
 
 const steps = [
   {
-    title: 'First',
-    content: <Personal />,
+    title: 'Gender',
+    content: <Gender />,
   },
   {
-    title: 'Second',
+    title: 'Categories',
     content: <Category />,
   },
   {
-    title: 'Last',
+    title: 'Keywords',
     content: <Keyword />,
   },
 ];
@@ -27,7 +30,8 @@ const steps = [
 function UserInfo() {
   const history = useHistory();
 
-  const [current, setCurrent] = React.useState(0);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const [current, setCurrent] = useState(0);
 
   const next = () => {
     setCurrent(current + 1);
@@ -38,27 +42,28 @@ function UserInfo() {
   };
 
   return (
-    <Container>
-      <HeaderComponent type="logo" />
-      <div style={{ width: '40%' }}>
-        <Steps
-          current={current}
-          labelPlacement="vertical"
-          style={{ marginBottom: '50px' }}
-        >
+    <>
+      {isAuthenticated ? (
+        <HeaderComponent type="logo" />
+      ) : (
+        <HeaderComponent type="logo guest" />
+      )}
+      <Wrapper>
+        <MySteps current={current} style={{ marginBottom: '50px' }}>
           {steps.map((item) => (
             <Step key={item.title} title={item.title} />
           ))}
-        </Steps>
+        </MySteps>
+
         <div
           className="steps-content"
           style={{
-            minHeight: '420px',
+            width: '100%',
+            height: '100%',
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'flex-start',
             alignItems: 'center',
-            fontFamily: 'Noto Sans KR',
             textAlign: 'left',
           }}
         >
@@ -66,14 +71,10 @@ function UserInfo() {
         </div>
         <div className="steps-action">
           {current < steps.length - 1 && (
-            <Button type="primary" onClick={() => next()}>
-              Next
-            </Button>
+            <Button onClick={() => next()}>Next</Button>
           )}
           {current === steps.length - 1 && (
-            <Button type="primary" onClick={() => history.push('/recommend')}>
-              Done
-            </Button>
+            <Button onClick={() => history.push('/recommend')}>Done</Button>
           )}
           {current > 0 && (
             <Button style={{ margin: 'auto' }} onClick={() => prev()}>
@@ -81,8 +82,8 @@ function UserInfo() {
             </Button>
           )}
         </div>
-      </div>
-    </Container>
+      </Wrapper>
+    </>
   );
 }
 
@@ -96,4 +97,60 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   text-align: center;
+`;
+
+const Wrapper = styled.div`
+  width: 40%;
+  height: calc(100% - 25rem);
+  margin: 20rem auto 5rem auto;
+  padding: 8rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  background: #ff6f00;
+  width: 10rem;
+  height: 4rem;
+  border: none;
+  border-radius: 0.5rem;
+  font-size: 1.7rem;
+  color: white;
+  margin: 0 3vw;
+  text-align: center;
+  vertical-align: middle;
+  display: table-cell;
+  line-height: 2;
+  margin: 5px 0;
+
+  :hover {
+    color: white;
+    box-shadow: 2px 4px 8px #ffb300;
+  }
+`;
+
+const MySteps = styled(Steps)`
+  .ant-steps-item-process > .ant-steps-item-container > .ant-steps-item-icon {
+    background-color: #ff6f00;
+  }
+  .ant-steps-item-process .ant-steps-item-icon {
+    border-color: #ff6f00;
+  }
+
+  .ant-steps-item-finish .ant-steps-item-icon {
+    border-color: #ff6f00;
+  }
+  .anticon {
+    color: #ff6f00;
+  }
+
+  .ant-steps-item-finish
+    > .ant-steps-item-container
+    > .ant-steps-item-content
+    > .ant-steps-item-title::after {
+    background-color: #ff6f00;
+  }
 `;
