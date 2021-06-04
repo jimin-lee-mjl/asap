cd2bc883e4d51e209c9f1a4727b253096914abcf
irisdew <isl9702@naver.com>
2021-06-04 17:23:00 +0900

    Feat: 결제 페이지 배송정보 입력 폼

---
+++ client/src/components/Payment/index.js
@@ -0,0 +1,131 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { Checkbox } from 'antd';
+import HeaderComponent from '../Header';
+
+export default function Payment() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const [delivery, setDelivery] = useState({
+    firstName: 'Harry',
+    lastName: 'Potter',
+    address: '77 N Kainalu Dr, Kailua, HI 96734',
+    post: 96734,
+  });
+
+  function onChange(e) {
+    console.log(`checked = ${e.target.checked}`);
+  }
+
+  return (
+    <>
+      {true ? (
+        <HeaderComponent type="logo" />
+      ) : (
+        <HeaderComponent type="logo guest" />
+      )}
+      <Container>
+        <DeliveryInfo>
+          <h1>Delivery Info</h1>
+          <form style={{ width: '70%', margin: 'auto' }}>
+            <FormControl>
+              <label>First Name</label>
+              <input
+                type="text"
+                value={delivery.firstName}
+                onChange={({ target: { value } }) => {
+                  setDelivery({ ...delivery, firstName: value });
+                }}
+              ></input>
+            </FormControl>
+            <FormControl>
+              <label>Last Name</label>
+              <input
+                type="text"
+                value={delivery.lastName}
+                onChange={({ target: { value } }) => {
+                  setDelivery({ ...delivery, lastName: value });
+                }}
+              ></input>
+            </FormControl>
+            <FormControl>
+              <label>Shipping Address </label>
+              <input
+                type="text"
+                value={delivery.address}
+                onChange={({ target: { value } }) => {
+                  setDelivery({ ...delivery, address: value });
+                }}
+              ></input>
+            </FormControl>
+            <FormControl>
+              <label>Postal Code</label>
+              <input
+                type="text"
+                value={delivery.post}
+                onChange={({ target: { value } }) => {
+                  setDelivery({ ...delivery, post: value });
+                }}
+              ></input>
+            </FormControl>
+          </form>
+          <Checkbox onChange={onChange}>
+            <h2>Save this Delivery Info</h2>
+          </Checkbox>
+        </DeliveryInfo>
+        <OrderInfo>
+          <div>
+            <h1>Your Order</h1>
+          </div>
+          <button>Pay with Paypal</button>
+        </OrderInfo>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div`
+  width: 70vw;
+  height: calc(100% - 25rem);
+  margin: 18rem auto 5rem auto;
+  display: flex;
+`;
+
+const DeliveryInfo = styled.div`
+  /* border: solid 0.1rem #ff6f00;
+  border-radius: 0.8rem; */
+  padding: 5rem;
+  width: 35vw;
+`;
+
+const OrderInfo = styled.div`
+  /* border: solid 0.1rem #ff6f00;
+  border-radius: 0.8rem; */
+  padding: 3rem 2rem;
+  width: 35vw;
+`;
+
+const FormControl = styled.div`
+  margin-bottom: 1rem;
+  padding-bottom: 0.5rem;
+  position: relative;
+
+  label {
+    color: #33032d;
+    display: block;
+    margin-bottom: 0.5rem;
+    text-align: left;
+    font-size: 1.5rem;
+  }
+
+  input {
+    border-radius: 0.5rem;
+    border: gray solid 0.2rem;
+    display: block;
+    width: 100%;
+    font-size: 1.8rem;
+    padding: 1rem;
+    margin-bottom: 1rem;
+  }
+`;
