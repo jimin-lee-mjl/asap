052b342a4454e704054d7839759b1795e5f48922
kkl <ycdi2223@naver.com>
2021-06-04 13:15:02 +0900

    Feat : 구매내역 상세페이지 ui

--- client/src/components/OrderHistory/index.js
+++ client/src/components/OrderHistory/index.js
@@ -74,34 +74,48 @@ export default function OrderHistory() {
     },
   };
 
-  const renderDeliveryInfo = () => {};
-
   return (
     <Container>
-      <div style={{ width: '1000px' }}>
+      <div style={{ alignSelf: 'start' }}>
         <h1>Order Details</h1>
       </div>
       <div>
-        <div>
-          <CartListTable
-            columns={columns}
-            dataSource={orderedItemsTableData}
-            scroll={{ y: 720 }}
-            style={{
-              width: '1000px',
-            }}
-            onRow={(record, index) => ({
-              onClick: () => {
-                console.log(record, index, 'clicked!!');
-              },
-            })}
-          />
-          <TableFooter>
-            <h1>Total ${}</h1>
-          </TableFooter>
-        </div>
+        <div style={{ textAlign: 'left' }}>2021-06-01 </div>
+        <CartListTable
+          columns={columns}
+          dataSource={orderedItemsTableData}
+          scroll={{ y: 400 }}
+          onRow={(
+            record,
+
+            index,
+          ) => ({
+            onClick: () => {
+              console.log(record, index, 'clicked!!');
+            },
+          })}
+        />
+        <TableFooter>
+          <h1>Total ${}</h1>
+        </TableFooter>
       </div>
-      <div>{renderDeliveryInfo()}</div>
+      <DeliveryInfoContainer>
+        <h1>Delivery Info</h1>
+        <DeliveryInfo>
+          <InfoTitleDiv>
+            <p>Name</p>
+            <p>Email</p>
+            <p>Shipping Address</p>
+            <p>Postal Code</p>
+          </InfoTitleDiv>
+          <InfoContentDiv>
+            <p>Kyunglim Khang</p>
+            <p>Kyunglim.Khang@gmail.com</p>
+            <p>157, Hwarang-ro, Seongbuk-gu, Seoul, Republic of Korea</p>
+            <p>02773</p>
+          </InfoContentDiv>
+        </DeliveryInfo>
+      </DeliveryInfoContainer>
     </Container>
   );
 }
@@ -111,8 +125,8 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 90vh;
-  margin-top: 100px;
+  width: 60vw;
+  margin: auto;
 `;
 
 const CartListTable = styled(Table)`
@@ -152,3 +166,29 @@ const ButtonGroup = styled.div`
     margin-left: 5px;
   }
 `;
+
+const DeliveryInfoContainer = styled.div`
+  width: 100%;
+  text-align: left;
+`;
+
+const DeliveryInfo = styled.div`
+  display: flex;
+  border: solid 1px;
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  align-items: center;
+`;
+
+const InfoTitleDiv = styled.div`
+  margin-left: 40px;
+  font-weight: bold;
+}`;
+const InfoContentDiv = styled.div`
+  margin-left: 40px;
+`;
+
+const InfoTitle = styled.div``;
+
+const InfoContent = styled.div``;
