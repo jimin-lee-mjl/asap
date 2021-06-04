de0ec3529caeaa188d4e74a55dc7b3e2816468a7
kimhyunkwang <khkhy8712@gmail.com>
2021-06-04 14:53:46 +0900

    Docs: API 문서 작성
    회원 탈퇴 API 문서 작성

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,18 +1,25 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework import status
 from django.contrib.auth import logout
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from .responses import SuccessResponse
 
 
-class UserDeleteView(APIView):
+class AccountDeleteView(APIView):
     permission_classes = [IsAuthenticated]
-    
+
+    account_delete_response = {
+        '200': openapi.Response(
+            description=SuccessResponse.account_deleted.SUCCESS_MSG
+        )
+    }
+
+    @swagger_auto_schema(responses=account_delete_response)
     def delete(self, request, format=None):
         user = request.user
         user.delete()
         logout(request)
-        content = {
-            'status': 'request was permitted'
-        }
-        return Response(content)
+        return Response(status=SuccessResponse.account_deleted.STATUS_CODE)
