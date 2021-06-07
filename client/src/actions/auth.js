49220bd193a7b2bccfa8c21d54c778c9809f97fd
irisdew <isl9702@naver.com>
2021-06-07 23:48:57 +0900

    Feat: 로그인 성공 시 home으로 redirect 추가, 실패 시 alert 추가

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -68,6 +68,11 @@ export const login = (username, password) => (dispatch) => {
     })
     .catch((err) => {
       console.log(err.response);
+      console.log(Object.keys(err.response.data));
+      const errors = Object.keys(err.response.data);
+      errors.forEach((x) => {
+        alert(err.response.data[x]);
+      });
       dispatch({
         type: LOGIN_FAIL,
       });
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,32 +1,19 @@
 import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, Redirect } from 'react-router-dom';
 import { login } from '../../actions/auth';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import Header from '../Header';
-
 import axios from 'axios';
 import { userinfo } from '../../actions/mypage';
 import baseUrl from '../../url';
+import { useSelector } from 'react-redux';
 
 export default function Login() {
   const dispatch = useDispatch();
   const history = useHistory();
-
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
-
-  const handleClick = () => {
-    try {
-      dispatch(login(id, password));
-      history.push('/');
-      getUser();
-    } catch (e) {
-      alert('Failed to login');
-      setId('');
-      setPassword('');
-    }
-  };
 
   const getUser = () => {
     const token = localStorage.getItem('token');
@@ -46,50 +33,63 @@ export default function Login() {
       .catch((err) => console.log(err.response));
   };
 
-  return (
-    <Container>
-      <Header type="guest" />
-      <Wrapper>
-        <Form>
-          {/* <h1 style={{ textAlign: 'center', margin: '0 0 2rem' }}>Login</h1> */}
-          <Logo
-            src="logo-circle.png"
-            alt="logo"
-            onClick={() => {
-              history.push('/');
-            }}
-          />
-          <FormControl>
-            <label>ID</label>
-            <input
-              type="test"
-              value={id}
-              onChange={({ target: { value } }) => setId(value)}
+  const handleClick = () => {
+    try {
+      dispatch(login(id, password));
+      getUser();
+    } catch (e) {
+      alert('Failed to login');
+      setId('');
+      setPassword('');
+    }
+  };
+
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  if (isAuthenticated) {
+    return <Redirect to="/" />;
+  } else {
+    return (
+      <Container>
+        <Header type="guest" />
+        <Wrapper>
+          <Form>
+            {/* <h1 style={{ textAlign: 'center', margin: '0 0 2rem' }}>Login</h1> */}
+            <Logo
+              src="logo-circle.png"
+              alt="logo"
+              onClick={() => {
+                history.push('/');
+              }}
             />
-          </FormControl>
-
-          <FormControl>
-            <label>Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={({ target: { value } }) => setPassword(value)}
-            />
-          </FormControl>
-
-          <Button type="primary" htmlType="submit" onClick={handleClick}>
-            Login
-          </Button>
-        </Form>
-
-        <Link to="/register" style={{ color: '#fb8c00' }}>
-          Create Account
-        </Link>
-      </Wrapper>
-    </Container>
-  );
+            <FormControl>
+              <label>ID</label>
+              <input
+                type="test"
+                value={id}
+                onChange={({ target: { value } }) => setId(value)}
+              />
+            </FormControl>
+            <FormControl>
+              <label>Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={({ target: { value } }) => setPassword(value)}
+              />
+            </FormControl>
+            <Button type="primary" htmlType="submit" onClick={handleClick}>
+              Login
+            </Button>
+          </Form>
+          <Link to="/register" style={{ color: '#fb8c00' }}>
+            Create Account
+          </Link>
+        </Wrapper>
+      </Container>
+    );
+  }
 }
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -97,18 +97,15 @@ const Container = styled.div`
   align-items: center;
   height: 90vh;
 `;
-
 const Logo = styled.img`
   width: 15rem;
   height: 15rem;
   margin: 3rem auto;
   cursor: pointer;
-
   :hover {
     filter: drop-shadow(2px 4px 8px #ffb300);
   }
 `;
-
 const Wrapper = styled.div`
   width: 40rem;
   min-height: 45rem;
@@ -119,25 +116,21 @@ const Wrapper = styled.div`
   margin: 2rem auto;
   text-align: center;
 `;
-
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   padding: 3rem 4rem;
   text-align: left;
 `;
-
 const FormControl = styled.div`
   margin-bottom: 1rem;
   padding-bottom: 0.5rem;
   position: relative;
-
   label {
     color: #33032d;
     display: block;
     margin-bottom: 0.5rem;
   }
-
   input {
     border-radius: 0.5rem;
     min-height: 2.618em;
@@ -148,7 +141,6 @@ const FormControl = styled.div`
     padding: 7px;
   }
 `;
-
 const Button = styled.div`
   background: black;
   color: white;
@@ -160,7 +152,6 @@ const Button = styled.div`
   border-radius: 0.5rem;
   border: solid black 0.2rem;
   margin-top: 1rem;
-
   :hover {
     color: black;
     background: white;
